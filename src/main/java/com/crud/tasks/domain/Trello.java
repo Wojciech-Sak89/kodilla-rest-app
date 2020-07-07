bc5dab664969dd8b7749835ae00f6a3f6700c575
Wojciech-Sak89 <wojciech.r.sak@gmail.com>
2020-07-07 13:22:33 +0200

    22.3 Zadanie: Rozszerzenie klasy odpowiedzi

--- src/main/java/com/crud/tasks/domain/Trello.java
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -6,20 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class Trello {
 
-    @JsonProperty("id")
-    private String id;
+    @JsonProperty("board")
+    private int board;
 
-    @JsonProperty("name")
-    private String name;
+    @JsonProperty("card")
+    private int card;
 
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
 }
