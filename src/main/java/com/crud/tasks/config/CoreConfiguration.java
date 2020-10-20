cf080176129156ea03d25381d07d61ae077ed044
Wojciech-Sak89 <wojciech.r.sak@gmail.com>
2020-10-20 10:57:25 +0200

    32.1 Zadanie: operacje matematyczne w Thymeleaf

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -12,8 +12,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @EnableScheduling
+@EnableSwagger2
 @Configuration
 public class CoreConfiguration implements WebMvcConfigurer {
 
