8faaa508492e5b4445e51306ae1f1955e41cc091
Wojciech-Sak89 <wojciech.r.sak@gmail.com>
2020-10-21 12:14:11 +0200

    32.2 Zadanie: Ustawienie pozostałych wartości

---
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -0,0 +1,39 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class MailCreatorService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    private CompanyConfig companyConfig;
+
+    @Autowired
+    @Qualifier("templateEngine")
+    private TemplateEngine templateEngine;
+
+    public String buildTrelloCardEmail(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("preview_message", "Created Trello Card INFO");
+        context.setVariable("goodbye_message",
+                "Thank you " + adminConfig.getAdminName() + " for using our services");
+        context.setVariable("company_details_name_and_goal",
+                companyConfig.getCompanyName() + "\u2122 tel.: " + companyConfig.getCompanyGoal());
+        context.setVariable("company_details_mail_and_phone",
+                companyConfig.getCompanyEmail() + " " + companyConfig.getCompanyPhone());
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+}
